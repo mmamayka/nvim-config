7a3426c4302e37bfe16955fdb8d701850b971a9b
Ilya Mamay <mmamayka01@gmail.com>
2026-04-09 17:27:49 +0300

    Remove dummy autocmds for nvim-tree
    Signed-off-by: Ilya Mamay <mmamayka01@gmail.com>

--- lua/specs/nvim-tree.lua
+++ lua/specs/nvim-tree.lua
@@ -60,59 +60,6 @@ return {
 
 			nvim_tree.setup(opts)
 
-			vim.api.nvim_create_autocmd({'BufEnter', 'QuitPre'}, {
-				nested = false,
-				callback = function(e)
-					local tree = require('nvim-tree.api').tree
-
-					-- Nothing to do if tree is not opened
-					if not tree.is_visible() then
-						return
-					end
-
-					-- How many focusable windows do we have? (excluding e.g. incline status window)
-					local winCount = 0
-					for _,winId in ipairs(vim.api.nvim_list_wins()) do
-						if vim.api.nvim_win_get_config(winId).focusable then
-							winCount = winCount + 1
-						end
-					end
-
-					-- We want to quit and only one window besides tree is left
-					if e.event == 'QuitPre' and winCount == 2 then
-						vim.api.nvim_cmd({cmd = 'qall'}, {})
-					end
-
-					-- :bd was probably issued an only tree window is left
-					-- Behave as if tree was closed (see `:h :bd`)
-					if e.event == 'BufEnter' and winCount == 1 then
-						-- Required to avoid "Vim:E444: Cannot close last window"
-						vim.defer_fn(function()
-							-- close nvim-tree: will go to the last buffer used before closing
-							tree.toggle({find_file = true, focus = true})
-							-- re-open nivm-tree
-							tree.toggle({find_file = true, focus = false})
-						end, 10)
-					end
-				end
-			})
-
-			local function maximize_and_preserve_tree()
-				local nvim_tree_view = require("nvim-tree.view")
-
-				vim.cmd("wincmd |")
-
-				if nvim_tree_view.is_visible() then
-					local width = nvim_tree_view.get_winnr() and nvim_tree_view.View.width
-					if width then
-						local current_win = vim.api.nvim_get_current_win()
-						vim.cmd("NvimTreeResize " .. width)
-						vim.api.nvim_set_current_win(current_win)
-					end
-				end
-
-			end
-			vim.keymap.set("n", "<C-w>|", maximize_and_preserve_tree, { desc = "Maximize window preserve nvim-tree" })
 			vim.keymap.set("n", "<leader>tt", '<cmd>NvimTreeToggle<CR>')
 			vim.keymap.set("n", "<leader>tf", '<cmd>NvimTreeFocus<CR>')
 		end,
